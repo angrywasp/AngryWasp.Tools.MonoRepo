3f81548ee95e7d59caddafc65743f0b15b30c939
angrywasp <angrywasp@live.com>
2022-12-15 23:56:48 +1100

    Update random number generators

--- AngryWasp.Helpers/RNG.cs
+++ AngryWasp.Helpers/RNG.cs
@@ -1,46 +1,31 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 namespace AngryWasp.Helpers
 {
-    public class MersenneTwister
+    /*public class MersenneTwister
     {
         private const int N = 624;
         private const int M = 397;
-        private const uint MATRIX_A = 0x9908b0df;
         private const uint UPPER_MASK = 0x80000000;
         private const uint LOWER_MASK = 0x7fffffff;
 
-        private const uint TEMPERING_MASK_B = 0x9d2c5680;
-        private const uint TEMPERING_MASK_C = 0xefc60000;
-
-        private static uint TEMPERING_SHIFT_U(uint y) => (y >> 11);
-        private static uint TEMPERING_SHIFT_S(uint y) => (y << 7);
-        private static uint TEMPERING_SHIFT_T(uint y) => (y << 15);
-        private static uint TEMPERING_SHIFT_L(uint y) => (y >> 18);
-
         private uint[] mt = new uint[N];
 
-        private uint seed;
         private short mti;
 
-        private static uint[] mag01 = { 0x0, MATRIX_A };
+        private static uint[] mag01 = { 0x0, 0x9908b0df };
 
-        public MersenneTwister(uint seed)
+        public MersenneTwister()
         {
-            Seed = seed;
-        }
+            var r = new byte[4];
+            RandomNumberGenerator.Create().GetNonZeroBytes(r);
+            var seed = BitShifter.ToUInt(r);
 
-        public uint Seed
-        {
-            set
-            {
-                seed = value;
-                mt[0] = seed & 0xffffffffU;
-                for (mti = 1; mti < N; mti++)
-                    mt[mti] = (69069 * mt[mti - 1]) & 0xffffffffU;
-            }
-
-            get => seed;
+            mt[0] = seed & 0xffffffffU;
+            for (mti = 1; mti < N; mti++)
+                mt[mti] = (69069 * mt[mti - 1]) & 0xffffffffU;
         }
 
         public uint GenerateUInt()
@@ -70,10 +55,10 @@ namespace AngryWasp.Helpers
             }
 
             y = mt[mti++];
-            y ^= TEMPERING_SHIFT_U(y);
-            y ^= TEMPERING_SHIFT_S(y) & TEMPERING_MASK_B;
-            y ^= TEMPERING_SHIFT_T(y) & TEMPERING_MASK_C;
-            y ^= TEMPERING_SHIFT_L(y);
+            y ^= (y >> 11);
+            y ^= (y << 7) & 0x9d2c5680;
+            y ^= (y << 15) & 0xefc60000;
+            y ^= (y >> 18);
 
             return y;
         }
@@ -103,28 +88,124 @@ namespace AngryWasp.Helpers
                 buffer[idx] = (byte)(this.GenerateUInt() / (uint.MaxValue / byte.MaxValue));
         }
 
+        public float NextFloat() => (float)this.GenerateUInt() / uint.MaxValue;
+
         public double NextDouble() => (double)this.GenerateUInt() / uint.MaxValue;
 
         public ulong NextULong() => (ulong)(NextDouble() * 1000000000000.0d);
-    }
+    }*/
 
-    public class XoShiRo256
+    public abstract class XoShiRo128
     {
-        private ulong[] state = null;
+        protected uint[] state = null;
 
-        public XoShiRo256(ulong[] state)
+        public XoShiRo128()
         {
-            if (state.Length != 4)
-                throw new InvalidOperationException("State must contain 4 elements");
+            var r = new byte[16];
+            var start = 0;
 
-            this.state = state;
+            RandomNumberGenerator.Create().GetNonZeroBytes(r);
+
+            state = new uint[] {
+                BitShifter.ToUInt(r, ref start),
+                BitShifter.ToUInt(r, ref start),
+                BitShifter.ToUInt(r, ref start),
+                BitShifter.ToUInt(r, ref start)
+            };
         }
 
-        private ulong Rotl64(ulong x, int k) => (x << k) | (x >> (64 - k));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected uint Rotl(uint x, int k) => (x << k) | (x >> (32 - k));
 
-        public ulong NextULong()
+        public abstract uint Next();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected virtual void Scramble()
         {
-            ulong result = Rotl64(state[1] * 5, 7) * 9;
+            uint t = state[1] << 9;
+
+            state[2] ^= state[0];
+            state[3] ^= state[1];
+            state[1] ^= state[2];
+            state[0] ^= state[3];
+
+            state[2] ^= t;
+            state[3] = Rotl(state[3], 11);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Next(int minValue, int maxValue) => (int)(this.Next() / ((float)uint.MaxValue / (maxValue - minValue)) + minValue);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public float NextFloat() => (float)this.Next() / uint.MaxValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void NextBytes(byte[] buffer)
+        {
+            for (int idx = 0; idx < buffer.Length; idx++)
+                buffer[idx] = (byte)(this.Next() / (uint.MaxValue / byte.MaxValue));
+        }
+    }
+
+    public class XoShiRo128Plus : XoShiRo128
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override uint Next()
+        {
+            uint result = state[0] + state[3];
+            Scramble();
+            return result;
+        }
+    }
+
+    public class XoShiRo128PlusPlus : XoShiRo128
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override uint Next()
+        {
+            uint result = Rotl(state[0] + state[3], 7) + state[0];
+            Scramble();
+            return result;
+        }
+    }
+
+    public class XoShiRo128StarStar : XoShiRo128
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override uint Next()
+        {
+            uint result = Rotl(state[1] * 5, 7) * 9;
+            Scramble();
+            return result;
+        }
+    }
+
+    public abstract class XoShiRo256
+    {
+        protected ulong[] state = null;
+
+        public XoShiRo256()
+        {
+            var r = new byte[32];
+            var start = 0;
+
+            RandomNumberGenerator.Create().GetNonZeroBytes(r);
+
+            state = new ulong[] {
+                BitShifter.ToULong(r, ref start),
+                BitShifter.ToULong(r, ref start),
+                BitShifter.ToULong(r, ref start),
+                BitShifter.ToULong(r, ref start)
+            };
+        }
+
+        protected ulong Rotl(ulong x, int k) => (x << k) | (x >> (64 - k));
+
+        public abstract ulong Next();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected void Scramble()
+        {
             ulong t = state[1] << 17;
 
             state[2] ^= state[0];
@@ -133,8 +214,41 @@ namespace AngryWasp.Helpers
             state[0] ^= state[3];
 
             state[2] ^= t;
-            state[3] = Rotl64(state[3], 45);
+            state[3] = Rotl(state[3], 45);
+        }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public long Next(long minValue, long maxValue) => (long)(this.Next() / ((double)ulong.MaxValue / (maxValue - minValue)) + minValue);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public double NextDouble() => (double)this.Next() / ulong.MaxValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void NextBytes(byte[] buffer)
+        {
+            for (int idx = 0; idx < buffer.Length; idx++)
+                buffer[idx] = (byte)(this.Next() / (ulong.MaxValue / byte.MaxValue));
+        }
+    }
+
+    public class XoShiRo256PlusPlus : XoShiRo256
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override ulong Next()
+        {
+            ulong result = Rotl(state[0] + state[3], 23) + state[0];
+            Scramble();
+            return result;
+        }
+    }
+
+    public class XoShiRo256StarStar : XoShiRo256
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override ulong Next()
+        {
+            ulong result = Rotl(state[1] * 5, 7) * 9;
+            Scramble();
             return result;
         }
     }
