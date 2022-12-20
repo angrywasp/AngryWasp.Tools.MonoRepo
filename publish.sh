35fd693bf5c9006d2a9a07f8bcff7d9e5820b35e
angrywasp <angrywasp@live.com>
2022-12-20 14:43:38 +1100

    Add script to publish nuget packages

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+export VERSION=3.0.1
+
+dotnet build -c Release Src/AngryWasp.Tools.MonoRepo.sln -p:PackageVersion=${VERSION}
+
+packages=(Cli Cryptography Helpers Json.Rpc Logger Net Serializer)
+
+for i in "${packages[@]}"
+do
+    dotnet nuget push Bin/Binaries/AngryWasp.${i}.${VERSION}.nupkg --source https://api.nuget.org/v3/index.json
+done
