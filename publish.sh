c4735c1d73e72ca809fbea4bcd8cfcae27d7539d
angrywasp <angrywasp@live.com>
2022-12-25 14:04:43 +1100

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,12 +1,29 @@
-#!/bin/bash -e
+#!/bin/bash
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export VERSION=3.0.2
+function getNewVersion()
+{
+    pkg=$1
+    lvl=$2
 
-dotnet build -c Release Src/AngryWasp.Tools.MonoRepo.sln -p:PackageVersion=${VERSION}
+    version=$(curl -s https://api.nuget.org/v3-flatcontainer/AngryWasp.${pkg}/index.json | jq .[][-1] | tr -d '"')
+    semver=(${version//./ })
 
-packages=(Cli Cryptography Helpers Json.Rpc Logger Net Serializer)
+    if [ "${lvl}" == "major" ]; then
+        ((semver[0]++))
+        semver[1]=0
+        semver[2]=0
+    elif [ "${lvl}" == "minor" ]; then
+        ((semver[1]++))
+        semver[2]=0
+    else
+        ((semver[2]++))
+    fi
 
-for i in "${packages[@]}"
-do
-    dotnet nuget push Bin/Binaries/AngryWasp.${i}.${VERSION}.nupkg --source https://api.nuget.org/v3/index.json
-done
+    echo "${semver[0]}.${semver[1]}.${semver[2]}"
+}
+
+version=$(getNewVersion $1 patch)
+
+dotnet build -c Release Src/AngryWasp.Tools.MonoRepo.sln -p:PackageVersion=${version}
+dotnet nuget push Bin/Binaries/AngryWasp.${1}.${version}.nupkg --source https://api.nuget.org/v3/index.json
